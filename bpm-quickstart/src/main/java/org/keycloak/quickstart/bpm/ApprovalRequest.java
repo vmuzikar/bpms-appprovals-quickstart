db180374afafa6ba7a5cfc08c908ed7f3e5bca5f
vmuzikar <vaclav@muzikari.cz>
2018-01-09 16:08:55 +0100

    ApprovalRequest impl

--- bpm-quickstart/src/main/java/org/keycloak/quickstart/bpm/ApprovalRequest.java
+++ bpm-quickstart/src/main/java/org/keycloak/quickstart/bpm/ApprovalRequest.java
@@ -1,35 +1,71 @@
 package org.keycloak.quickstart.bpm;
 
+import org.apache.http.Header;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.methods.RequestBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicHeader;
+
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ApprovalRequest implements java.io.Serializable {
 
     static final long serialVersionUID = 1L;
 
-    private String url;
+    private String keycloakRootUrl;
+    private String realm;
+    private String approvalId;
     private String accessToken;
 
-    public ApprovalRequest(String url, String accessToken) {
-        this.url = url;
+    public ApprovalRequest(String keycloakRootUrl, String realm, String approvalId, String accessToken) {
+        this.keycloakRootUrl = keycloakRootUrl;
+        this.realm = realm;
+        this.approvalId = approvalId;
         this.accessToken = accessToken;
     }
 
-    public String getUrl() {
-        return url;
+    public String getKeycloakRootUrl() {
+        return keycloakRootUrl;
     }
 
-    public void setUrl(String url) {
-        this.url = url;
+    public String getRealm() {
+        return realm;
     }
 
-    public String getDescription() {
-        return "Some description";
+    public String getApprovalId() {
+        return approvalId;
     }
 
-    public void approve() {
-        System.out.println("Request approved; token: " + accessToken);
+    public String getAccessToken() {
+        return accessToken;
     }
 
-    public void reject() {
+    public void approve() throws IOException {
+        HttpUriRequest request = RequestBuilder.post(getApprovalUrl()).build();
+        getHttpClient().execute(request);
+    }
 
-        System.out.println("Request rejected; token: " + accessToken);
+    public void reject() throws IOException {
+        HttpUriRequest request = RequestBuilder.delete(getApprovalUrl()).build();
+        getHttpClient().execute(request);
+    }
+
+    private HttpClient getHttpClient() {
+        List<Header> headers = new ArrayList<Header>();
+        headers.add(new BasicHeader(HttpHeaders.CONTENT_TYPE, "application/json"));
+        headers.add(new BasicHeader(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken));
+
+        return HttpClientBuilder.create()
+                .setDefaultHeaders(headers)
+                .build();
+    }
+
+    private URI getApprovalUrl() {
+        return URI.create(String.format("%s/admin/%s/approvals/%s", keycloakRootUrl, realm, approvalId));
     }
 }
